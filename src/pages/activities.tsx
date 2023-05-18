689918fef2d19021b78abcfaddc94b8dbfea7937
Ploy <117616170+yaminP@users.noreply.github.com>
2023-05-18 14:47:57 +0700

    card-gallery

--- src/pages/activities.tsx
+++ src/pages/activities.tsx
@@ -7,7 +7,7 @@ export default function Gallery() {
             <h3>ภาพกิจกรรม</h3>
             <div className='container d-flex flex-wrap align-content-around'>
                 {gallery.map((item) => (
-                    <div key={item.id} className='card m-2'>
+                    <div key={item.id} className='card-gallery m-2'>
                         <img className="img-gallery" src={item.img} alt="" />
                     </div>
                 ))}
