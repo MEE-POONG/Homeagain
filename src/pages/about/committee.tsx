e744441688f7a36b8effca3b3d577247dac0c8b1
Ploy <117616170+yaminP@users.noreply.github.com>
2023-05-31 17:28:08 +0700

    commit

--- src/pages/about/committee.tsx
+++ src/pages/about/committee.tsx
@@ -8,32 +8,103 @@ export default function Commitee() {
                     <div className="py-4 origin-section">
                         <p className="display-5">คณะกรรมการของมูลนิธิฯ</p>
                     </div>
-                    <div className=" m-sm-3 m-lg-5 shadow-sm">
-                        <img src="/images/Organizational-Chart.png" className="w-lg-75 w-100" alt="" />
-                    </div>
-
-                    <div className="row people">
-                        <div className="col-md-6 col-lg-4 item">
-                            <div className="box">
-                                <h4>Assistant Manager</h4>
-                                <p className="title">ผู้ช่วยผู้จัดการ</p>
-                                <img className=" rounded-circle" src="/images/adisak.jpg" alt="" />
-                                <h4 className="name">นายอดิศักดิ์ ก่อแก้ว</h4>
-                                <p className="title">Adisak Korkeaw</p>
+                    <div className="container">
+                        <div className="row row-cols-2 row-cols-lg-5 g-2 g-lg-4 people">
+                            <div className="col item">
+                                <div className="p-1 box shadow">
+                                    <h4 className=" text-uppercase">Manager</h4>
+                                    <p className="title">ผู้จัดการ</p>
+                                    <img className=" rounded-circle " src="https://imagedelivery.net/QZ6TuL-3r02W7wQjQrv5DA/947130a0-adde-4905-9003-48e461328800/300" alt="" />
+                                    <h3 className="name">คุณณัฏฐา ชื่นชม</h3>
+                                    <p className="title">Nattha  Chuenchom</p>
+                                </div>
                             </div>
-                        </div>
 
-                        <div className="col-md-6 col-lg-4 item">
-                            <div className="box">
-                                <h4>Treasurer</h4>
-                                <p className="title">เหรัญญิก</p>
-                                <img className=" rounded-circle" src="/images/nattha.jpg" alt="" />
-                                <h4 className="name">นางสาวณัฐฐาพร เงินคำ</h4>
-                                <p className="title">
-Natthaphon Ngoenkham</p>
+                            <div className="col item">
+                                <div className="p-1 box shadow">
+                                    <h4 className=" text-uppercase">Assistant Manager</h4>
+                                    <p className="title">ผู้ช่วยผู้จัดการ</p>
+                                    <img className=" rounded-circle " src="/images/adisak.jpg" alt="" />
+                                    <h3 className="name">นายอดิศักดิ์ ก่อแก้ว</h3>
+                                    <p className="title">Adisak Korkeaw</p>
+                                </div>
                             </div>
-                        </div>
 
+                            <div className="col item">
+                                <div className="p-1 box shadow">
+                                    <h4 className=" text-uppercase">Cheif</h4>
+                                    <p className="title">หัวหน้าศูนย์หนองบุญมาก</p>
+                                    <img className=" rounded-circle " src="https://imagedelivery.net/QZ6TuL-3r02W7wQjQrv5DA/947130a0-adde-4905-9003-48e461328800/300" alt="" />
+                                    <h3 className="name">คุณประวิทย์  ขุมเงิน</h3>
+                                    <p className="title">Prawit  Kumngoen</p>
+                                </div>
+                            </div>
+
+                            <div className="col item">
+                                <div className="p-1 box shadow">
+                                    <h4 className=" text-uppercase">Veterinary</h4>
+                                    <p className="title">สัตวแพทย์</p>
+                                    <img className=" rounded-circle " src="https://imagedelivery.net/QZ6TuL-3r02W7wQjQrv5DA/947130a0-adde-4905-9003-48e461328800/300" alt="" />
+                                    <h3 className="name">ดร.สถาพร  อันอามาตย์</h3>
+                                    <p className="title">Dr.Sathaporn Anammat</p>
+                                </div>
+                            </div>
+                            <div className="col item">
+                                <div className="p-1 box shadow">
+                                    <h4 className=" text-uppercase">DIRECTOR</h4>
+                                    <p className="title">คณะกรรมการ</p>
+                                    <img className=" rounded-circle " src="https://imagedelivery.net/QZ6TuL-3r02W7wQjQrv5DA/947130a0-adde-4905-9003-48e461328800/300" alt="" />
+                                    <h3 className="name">คุณยนัสฐา นาคเกิด</h3>
+                                    <p className="title">Chanutha Narkgerd</p>
+                                </div>
+                            </div>
+                            <div className="col item">
+                                <div className="p-1 box shadow">
+                                    <h4 className=" text-uppercase">DIRECTOR</h4>
+                                    <p className="title">คณะกรรมการ</p>
+                                    <img className=" rounded-circle " src="https://imagedelivery.net/QZ6TuL-3r02W7wQjQrv5DA/947130a0-adde-4905-9003-48e461328800/300" alt="" />
+                                    <h3 className="name">คุณสุดา เหรียญมณี</h3>
+                                    <p className="title">Suda Rianmanee</p>
+                                </div>
+                            </div>
+                            <div className="col item">
+                                <div className="p-1 box shadow">
+                                    <h4 className=" text-uppercase">PUCLIC RELATION</h4>
+                                    <p className="title">ฝ่ายประชาสมพันธ์</p>
+                                    <img className=" rounded-circle " src="https://imagedelivery.net/QZ6TuL-3r02W7wQjQrv5DA/947130a0-adde-4905-9003-48e461328800/300" alt="" />
+                                    <h3 className="name">คุณลลิตา สินทวงศ์ ณ อยุทธยา</h3>
+                                    <p className="title">Lalda Snidwongs</p>
+                                </div>
+                            </div>
+                            <div className="col item">
+                                <div className="p-1 box shadow">
+                                    <h4 className=" text-uppercase">PUCLIC RELATION</h4>
+                                    <p className="title">ฝ่ายประชาสมพันธ์</p>
+                                    <img className=" rounded-circle " src="https://imagedelivery.net/QZ6TuL-3r02W7wQjQrv5DA/947130a0-adde-4905-9003-48e461328800/300" alt="" />
+                                    <h3 className="name">ไทยแอนนา เวย์เม้าท์</h3>
+                                    <p className="title">Thaina Weymouth</p>
+                                </div>
+                            </div>
+                            <div className="col item">
+                                <div className="p-1 box shadow">
+                                    <h4 className=" text-uppercase">Treasurer</h4>
+                                    <p className="title">เหรัญญิก</p>
+                                    <img className=" rounded-circle " src="/images/nattha.jpg" alt="" />
+                                    <h3 className="name">คุณณัฐฐาพร เงินคำ</h3>
+                                    <p className="title">Natthaphon Ngoenkham</p>
+                                </div>
+                            </div>
+                            <div className="col item">
+                                <div className="p-1 box shadow">
+                                    <h4 className=" text-uppercase">WEBSITE</h4>
+                                    <p className="title">ดูแลเว็บไซต์</p>
+                                    <img className=" rounded-circle " src="https://imagedelivery.net/QZ6TuL-3r02W7wQjQrv5DA/947130a0-adde-4905-9003-48e461328800/300" alt="" />
+                                    <h3 className="name">คุณทศพร ดีสุทธิ</h3>
+                                    <p className="title">คุณทศพร ดีสุทธิ</p>
+                                </div>
+                            </div>
+                            
+                        </div>
                     </div>
 
                 </section>
