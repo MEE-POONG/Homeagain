98e2427169bc6a9a6025a38408cff67dcbfc7928
Ploy <117616170+yaminP@users.noreply.github.com>
2023-06-01 12:39:26 +0700

    committ

--- src/pages/about/committee.tsx
+++ src/pages/about/committee.tsx
@@ -5,13 +5,49 @@ export default function Commitee() {
         <>
             <LayOut>
                 <section className="about-section container text-center py-5 ">
-                    <div className="py-4 origin-section">
+                    <div className="py-5 origin-section">
                         <p className="display-5">คณะกรรมการของมูลนิธิฯ</p>
                     </div>
-                    <div className="container">
+                    <div className="container mt-5">
+                        <div className="row g-2 g-lg-0">
+                            <div className="row mx-auto item g-3 g-md-4 ">
+                                <div className="col-md-5 text-md-end ">
+                                        <img className=" rounded-circle " src="https://imagedelivery.net/QZ6TuL-3r02W7wQjQrv5DA/947130a0-adde-4905-9003-48e461328800/300" alt="" />
+                                </div>
+                                <div className="col-md-7 text-md-start px-md-3">
+                                    <h4 className=" text-uppercase d-inline">ADVISOR </h4>
+                                    <p className="title d-inline">ที่ปรึกษา</p>
+                                    <h4 className="">พระวิเทศสิทธิธรรมมาภรณ์ (เจ้าคุณฤทธิ์)</h4>
+                                    <h4 className="m-0 title">CHAO KHUN RIT</h4>
+                                    <p className="mt-3 title text-reset">เจ้าอาวาสวัดอดัมมยตาราม</p>
+                                    <p className="title text-reset"> ผู้ก่อตั้งศูนย์ปฏิบัติธรรม Northwest Meditation Center</p>
+                                </div>
+                            </div>
+                            <div className="col-md-6 item">
+                                <div className=" ">
+                                    <h4 className=" text-uppercase">PRESIDENT</h4>
+                                    <p className="title">ประธานมูลนิธิๆ</p>
+                                    <img className=" rounded-circle " src="https://imagedelivery.net/QZ6TuL-3r02W7wQjQrv5DA/947130a0-adde-4905-9003-48e461328800/300" alt="" />
+                                    <h3 className="name">คุณพนอจิตต์ จิตต์สุวรรณ</h3>
+                                    <p className="title">Panorchit Chitsuwan Waymouth</p>
+                                </div>
+                            </div>
+                            <div className="col-md-6 item">
+                                <div className=" ">
+                                    <h4 className=" text-uppercase">VICE PRESIDENT </h4>
+                                    <p className="title">รองประธาน</p>
+                                    <img className=" rounded-circle " src="https://imagedelivery.net/QZ6TuL-3r02W7wQjQrv5DA/947130a0-adde-4905-9003-48e461328800/300" alt="" />
+                                    <h3 className="name">คุณบัณฑิตทัศน์ ทสยันไชย</h3>
+                                    <p className="title">Bandidtat Tosayanchai</p>
+                                </div>
+                            </div>
+                        </div>
+                        <hr />
+
+                        {/* คณะกรรมการ */}
                         <div className="row row-cols-2 row-cols-lg-5 g-2 g-lg-4 people">
                             <div className="col item">
-                                <div className="p-1 box shadow">
+                                <div className="p-1 shadow-sm">
                                     <h4 className=" text-uppercase">Manager</h4>
                                     <p className="title">ผู้จัดการ</p>
                                     <img className=" rounded-circle " src="https://imagedelivery.net/QZ6TuL-3r02W7wQjQrv5DA/947130a0-adde-4905-9003-48e461328800/300" alt="" />
@@ -21,17 +57,17 @@ export default function Commitee() {
                             </div>
 
                             <div className="col item">
-                                <div className="p-1 box shadow">
+                                <div className="p-1 shadow-sm">
                                     <h4 className=" text-uppercase">Assistant Manager</h4>
                                     <p className="title">ผู้ช่วยผู้จัดการ</p>
                                     <img className=" rounded-circle " src="/images/adisak.jpg" alt="" />
-                                    <h3 className="name">นายอดิศักดิ์ ก่อแก้ว</h3>
+                                    <h3 className="name">คุณอดิศักดิ์ ก่อแก้ว</h3>
                                     <p className="title">Adisak Korkeaw</p>
                                 </div>
                             </div>
 
                             <div className="col item">
-                                <div className="p-1 box shadow">
+                                <div className="p-1 shadow-sm">
                                     <h4 className=" text-uppercase">Cheif</h4>
                                     <p className="title">หัวหน้าศูนย์หนองบุญมาก</p>
                                     <img className=" rounded-circle " src="https://imagedelivery.net/QZ6TuL-3r02W7wQjQrv5DA/947130a0-adde-4905-9003-48e461328800/300" alt="" />
@@ -41,7 +77,7 @@ export default function Commitee() {
                             </div>
 
                             <div className="col item">
-                                <div className="p-1 box shadow">
+                                <div className="p-1 shadow-sm">
                                     <h4 className=" text-uppercase">Veterinary</h4>
                                     <p className="title">สัตวแพทย์</p>
                                     <img className=" rounded-circle " src="https://imagedelivery.net/QZ6TuL-3r02W7wQjQrv5DA/947130a0-adde-4905-9003-48e461328800/300" alt="" />
@@ -49,8 +85,9 @@ export default function Commitee() {
                                     <p className="title">Dr.Sathaporn Anammat</p>
                                 </div>
                             </div>
+
                             <div className="col item">
-                                <div className="p-1 box shadow">
+                                <div className="p-1 shadow-sm">
                                     <h4 className=" text-uppercase">DIRECTOR</h4>
                                     <p className="title">คณะกรรมการ</p>
                                     <img className=" rounded-circle " src="https://imagedelivery.net/QZ6TuL-3r02W7wQjQrv5DA/947130a0-adde-4905-9003-48e461328800/300" alt="" />
@@ -58,8 +95,9 @@ export default function Commitee() {
                                     <p className="title">Chanutha Narkgerd</p>
                                 </div>
                             </div>
+
                             <div className="col item">
-                                <div className="p-1 box shadow">
+                                <div className="p-1 shadow-sm">
                                     <h4 className=" text-uppercase">DIRECTOR</h4>
                                     <p className="title">คณะกรรมการ</p>
                                     <img className=" rounded-circle " src="https://imagedelivery.net/QZ6TuL-3r02W7wQjQrv5DA/947130a0-adde-4905-9003-48e461328800/300" alt="" />
@@ -67,8 +105,9 @@ export default function Commitee() {
                                     <p className="title">Suda Rianmanee</p>
                                 </div>
                             </div>
+
                             <div className="col item">
-                                <div className="p-1 box shadow">
+                                <div className="p-1 shadow-sm">
                                     <h4 className=" text-uppercase">PUCLIC RELATION</h4>
                                     <p className="title">ฝ่ายประชาสมพันธ์</p>
                                     <img className=" rounded-circle " src="https://imagedelivery.net/QZ6TuL-3r02W7wQjQrv5DA/947130a0-adde-4905-9003-48e461328800/300" alt="" />
@@ -76,8 +115,9 @@ export default function Commitee() {
                                     <p className="title">Lalda Snidwongs</p>
                                 </div>
                             </div>
+
                             <div className="col item">
-                                <div className="p-1 box shadow">
+                                <div className="p-1 shadow-sm">
                                     <h4 className=" text-uppercase">PUCLIC RELATION</h4>
                                     <p className="title">ฝ่ายประชาสมพันธ์</p>
                                     <img className=" rounded-circle " src="https://imagedelivery.net/QZ6TuL-3r02W7wQjQrv5DA/947130a0-adde-4905-9003-48e461328800/300" alt="" />
@@ -85,8 +125,9 @@ export default function Commitee() {
                                     <p className="title">Thaina Weymouth</p>
                                 </div>
                             </div>
+
                             <div className="col item">
-                                <div className="p-1 box shadow">
+                                <div className="p-1 shadow-sm">
                                     <h4 className=" text-uppercase">Treasurer</h4>
                                     <p className="title">เหรัญญิก</p>
                                     <img className=" rounded-circle " src="/images/nattha.jpg" alt="" />
@@ -94,21 +135,22 @@ export default function Commitee() {
                                     <p className="title">Natthaphon Ngoenkham</p>
                                 </div>
                             </div>
+
                             <div className="col item">
-                                <div className="p-1 box shadow">
+                                <div className="p-1 shadow-sm">
                                     <h4 className=" text-uppercase">WEBSITE</h4>
                                     <p className="title">ดูแลเว็บไซต์</p>
                                     <img className=" rounded-circle " src="https://imagedelivery.net/QZ6TuL-3r02W7wQjQrv5DA/947130a0-adde-4905-9003-48e461328800/300" alt="" />
                                     <h3 className="name">คุณทศพร ดีสุทธิ</h3>
-                                    <p className="title">คุณทศพร ดีสุทธิ</p>
+                                    <p className="title">Tossapom Doesutti</p>
                                 </div>
                             </div>
-                            
+
                         </div>
                     </div>
 
                 </section>
-            </LayOut>
+            </LayOut >
         </>
     )
 }
