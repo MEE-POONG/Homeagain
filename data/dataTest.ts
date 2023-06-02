a613e3da6c57aad36671627d0f22adb911d1bb45
devilzeros <devilzeros@outlook.co.th>
2023-06-02 18:04:01 +0700

    east

--- data/dataTest.ts
+++ data/dataTest.ts
@@ -1,20 +1,37 @@
-export const dogDetail = [ 
-    { id: '1', image:'/images/FB-ถั่วแดง.jpg',dogName: 'ถั่วแดง', character: 'ขี้เล่น ร่าเริง', sex: 'เพศเมีย',dateofbirth:'-', old: '2ปี', story:'น้องถั่วแดง เป็นสุนัขที่แม่ของน้องคลอดแล้วทิ้งไว้ข้างกองขยะ....', dateCome:'20 พ.ค. 2563'},
-    { id: '2', image:'/images/FB-ถั่วดิน.jpg',dogName: 'ถั่วดิน', character: 'ขี้เล่น ร่าเริง', sex: 'เพศเมีย', old: '1ปี', story:'น้องถั่วแดง เป็นสุนัขที่แม่ของน้องคลอดแล้วทิ้งไว้ข้างกองขยะ....'},
-    { id: '3', image:'/images/FB-ถั่วเหลือง.jpg',dogName: 'ถั่วเหลือง', character: 'ขี้เล่น ร่าเริง', sex: 'เพศเมีย', old: '1ปี กับ 5เดือน ', story:'น้องถั่วแดง เป็นสุนัขที่แม่ของน้องคลอดแล้วทิ้งไว้ข้างกองขยะ....'},
-    { id: '4', image:'/images/FB-ถั่วแปป.jpg',dogName: 'ถั่วแปป', character: 'ขี้เล่น ร่าเริง', sex: 'เพศผู้', old: '1ปี กับ 5เดือน ', story:'น้องถั่วแดง เป็นสุนัขที่แม่ของน้องคลอดแล้วทิ้งไว้ข้างกองขยะ....'},
+export const dogDetailTH = [
+    { id: '1', image: '/images/FB-ถั่วแดง.jpg', dogName: 'ถั่วแดง', character: 'ขี้เล่น ร่าเริง', sex: 'เพศเมีย', dateofbirth: '-', old: '2ปี', story: 'น้องถั่วแดง เป็นสุนัขที่แม่ของน้องคลอดแล้วทิ้งไว้ข้างกองขยะ....', dateCome: '20 พ.ค. 2563' },
+    { id: '2', image: '/images/FB-ถั่วดิน.jpg', dogName: 'ถั่วดิน', character: 'ขี้เล่น ร่าเริง', sex: 'เพศเมีย', old: '1ปี', story: 'น้องถั่วแดง เป็นสุนัขที่แม่ของน้องคลอดแล้วทิ้งไว้ข้างกองขยะ....' },
+    { id: '3', image: '/images/FB-ถั่วเหลือง.jpg', dogName: 'ถั่วเหลือง', character: 'ขี้เล่น ร่าเริง', sex: 'เพศเมีย', old: '1ปี กับ 5เดือน ', story: 'น้องถั่วแดง เป็นสุนัขที่แม่ของน้องคลอดแล้วทิ้งไว้ข้างกองขยะ....' },
+    { id: '4', image: '/images/FB-ถั่วแปป.jpg', dogName: 'ถั่วแปป', character: 'ขี้เล่น ร่าเริง', sex: 'เพศผู้', old: '1ปี กับ 5เดือน ', story: 'น้องถั่วแดง เป็นสุนัขที่แม่ของน้องคลอดแล้วทิ้งไว้ข้างกองขยะ....' },
 ];
 
 export const slider = [
-    {id:'1', img:'/images/7-scaled.jpg'},
-    {id:'2', img:'/images/1-scaled.jpg'},
-    {id:'3', img:'/images/2-scaled.jpg'},
-    {id:'4', img:'/images/design.png'}
+    { id: '1', img: '/images/7-scaled.jpg' },
+    { id: '2', img: '/images/1-scaled.jpg' },
+    { id: '3', img: '/images/2-scaled.jpg' },
+    { id: '4', img: '/images/design.png' }
 ];
 
 export const gallery = [
-    {id:'1', img:'/images/activity/at02.jpg'},
-    {id:'2', img:'/images/activity/at03.jpg'},
-    {id:'3', img:'/images/activity/at04.jpg'},
-    {id:'4', img:'/images/activity/at01.jpg'},
+    { id: '1', img: '/images/activity/at02.jpg' },
+    { id: '2', img: '/images/activity/at03.jpg' },
+    { id: '3', img: '/images/activity/at04.jpg' },
+    { id: '4', img: '/images/activity/at01.jpg' },
+]
+export const committee = [
+    { id: '1', img: 'https://imagedelivery.net/QZ6TuL-3r02W7wQjQrv5DA/fb9efe15-dde2-45d7-2d34-65446db0e700/300', nameTH: "พระวิเทศสิทธิธรรมมาภรณ์ (เจ้าคุณฤทธิ์) เจ้าอาวาสวัดอดัมมยตาราม", nameEN: "CHAO KHUN RIT", positionTH: "ที่ปรึกษา", positionEN: "ADVISOR", detailTH: "ผู้ก่อตั้งศูนย์ปฏิบัติธรรม Northwest Meditation Center", detailEN: "", level: "1" },
+    { id: '2', img: 'https://imagedelivery.net/QZ6TuL-3r02W7wQjQrv5DA/8e44fcbf-3a60-4941-0e69-03b080ad4800/300', nameTH: "คุณพนอจิตต์ จิตต์สุวรรณ", nameEN: "Panorchit Chitsuwan Waymouth", positionTH: "ประธาน", positionEN: "PRESIDENT", detailTH: "", detailEN: "", level: "1" },
+    { id: '3', img: 'https://imagedelivery.net/QZ6TuL-3r02W7wQjQrv5DA/230ea2e9-44c9-434f-96ec-649076385900/300', nameTH: "คุณบัณฑิตทัศน์ ทสยันไชย", nameEN: "Bandidtat Tosayanchai", positionTH: "รองประธาน", positionEN: "VICE PRESIDENT", detailTH: "", detailEN: "", level: "1" },
+    { id: '4', img: 'https://imagedelivery.net/QZ6TuL-3r02W7wQjQrv5DA/00b98078-2f76-4e24-abdd-d788e11a7700/300', nameTH: "คุณณัฏฐา ชื่นชม", nameEN: "Nattha  Chuenchom", positionTH: "กรรมการ / ผู้จัดการ", positionEN: "MANAGER", detailTH: "", detailEN: "", level: "2" },
+    { id: '5', img: 'https://imagedelivery.net/QZ6TuL-3r02W7wQjQrv5DA/87888cc2-a8b6-488e-2fa1-59cb505ee700/300', nameTH: "คุณอดิศักดิ์ ก่อแก้ว", nameEN: "Adisak Korkeaw", positionTH: "ผู้ช่วยผู้จัดการ", positionEN: "ASSISTANT MANAGER", detailTH: "", detailEN: "", level: "2" },
+    { id: '6', img: 'https://imagedelivery.net/QZ6TuL-3r02W7wQjQrv5DA/408dd58d-54ac-4e51-70eb-396d54ca3f00/300', nameTH: "ดร.สถาพร อันอามาตย์", nameEN: "Dr.Sathaporn Anammat", positionTH: "สัตวแพทย์", positionEN: "VETERINARY", detailTH: "", detailEN: "", level: "3" },
+    { id: '7', img: 'https://imagedelivery.net/QZ6TuL-3r02W7wQjQrv5DA/1b46085c-1d13-4c24-64d1-d3168bd9ef00/300', nameTH: "คุณยนัสฐา นาคเกิด", nameEN: "Chanutha Narkgerd", positionTH: "กรรมการมูลนิธิบ้านหลังใหม่", positionEN: "DIRECTOR", detailTH: "", detailEN: "", level: "3" },
+    { id: '8', img: 'https://imagedelivery.net/QZ6TuL-3r02W7wQjQrv5DA/87a0e2b3-543d-425d-df37-8f7abb1e1400/300', nameTH: "คุณสุดา เหรียญมณี", nameEN: "Suda Rianmanee", positionTH: "กรรมการมูลนิธิบ้านหลังใหม่", positionEN: "DIRECTOR", detailTH: "", detailEN: "", level: "3" },
+    { id: '9', img: 'https://imagedelivery.net/QZ6TuL-3r02W7wQjQrv5DA/e76f3435-3e41-4f79-3300-dc91d6571000/300', nameTH: "คุณลักษณาวดี สินทวงศ์ ณ อยุทธยา", nameEN: "Luxanavadee Snidwongs", positionTH: "ฝ่ายประชาสมพันธ์", positionEN: "PUCLIC RELATION", detailTH: "", detailEN: "", level: "3" },
+    { id: '10', img: 'https://imagedelivery.net/QZ6TuL-3r02W7wQjQrv5DA/d3130374-84ad-41ba-2aa2-15e293425100/300', nameTH: "คุณลลิตา สินทวงศ์ ณ อยุทธยา", nameEN: "Lalda Snidwongs", positionTH: "ฝ่ายประชาสมพันธ์", positionEN: "PUCLIC RELATION", detailTH: "", detailEN: "", level: "3" },
+    { id: '11', img: 'https://imagedelivery.net/QZ6TuL-3r02W7wQjQrv5DA/8dc94b37-623b-41ce-9214-fa6e23596e00/300', nameTH: "ไทยแอนนา เวย์เม้าท์", nameEN: "Thaina Weymouth", positionTH: "ฝ่ายประชาสมพันธ์", positionEN: "PUCLIC RELATION", detailTH: "", detailEN: "", level: "3" },
+    { id: '12', img: 'https://imagedelivery.net/QZ6TuL-3r02W7wQjQrv5DA/3839f5a4-fbb7-4a45-2223-55a0d2178900/300', nameTH: "คุณคณิษฐ์นันท์ อิศรางกูร ณ อยุทธยา", nameEN: "Kanisnan Isarangkun", positionTH: "ฝ่ายประชาสมพันธ์", positionEN: "PUCLIC RELATION", detailTH: "", detailEN: "", level: "3" },
+    { id: '13', img: 'https://imagedelivery.net/QZ6TuL-3r02W7wQjQrv5DA/67d707e8-8cd8-48d5-291b-13822fab8700/300', nameTH: "คุณณัฐฐาพร เงินคำ", nameEN: "Natthaphon Ngoenkham", positionTH: "เหรัญญิก", positionEN: "TREASURER", detailTH: "", detailEN: "", level: "3" },
+    { id: '14', img: 'https://imagedelivery.net/QZ6TuL-3r02W7wQjQrv5DA/98d3128c-7d0b-42dd-4767-4794ee05c000/300', nameTH: "คุณทศพร ดีสุทธิ", nameEN: "Tossapom Doesutti", positionTH: "ดูแลเว็บไซต์", positionEN: "WEBSITE", detailTH: "", detailEN: "", level: "3" },
+
 ]
--- src/pages/about/committee.tsx
+++ src/pages/about/committee.tsx
@@ -1,7 +1,11 @@
 import { Image, Row } from "react-bootstrap";
 import LayOut from "../../../components/LayOut";
+import { committee } from "../../../data/dataTest";
 
-export default function Commitee() {
+const Committee: React.FC = () => {
+    const committeeLevel = (level) => {
+        return committee.filter((member) => member.level === level);
+    };
     return (
         <>
             <LayOut>
@@ -10,176 +14,58 @@ export default function Commitee() {
                         <p className="display-5">คณะกรรมการของมูลนิธิฯ</p>
                     </div>
                     <div className="committee">
-                        <div className="row lv-1 align-items-center">
-                            <div className="col-xl-6 col-lg-6 col-md-12">
-                                <div className="d-flex people-list ADVISOR px-3 py-4 my-3">
-                                    <Image className="rounded-circle" src="https://imagedelivery.net/QZ6TuL-3r02W7wQjQrv5DA/fb9efe15-dde2-45d7-2d34-65446db0e700/300" />
-                                    <div className="w-100  m-auto">
-                                        <h4 className=" text-uppercase ">ADVISOR ที่ปรึกษา</h4>
-                                        <h6 className="bold">พระวิเทศสิทธิธรรมมาภรณ์ (เจ้าคุณฤทธิ์)</h6>
-                                        <h6 className="bold">เจ้าอาวาสวัดอดัมมยตาราม</h6>
-                                        <p className="title text-reset"> ผู้ก่อตั้งศูนย์ปฏิบัติธรรม Northwest Meditation Center</p>
-                                        <h4 className=" text-uppercase ">CHAO KHUN RIT</h4>
-                                    </div>
+                        {['1', '2', '3'].map((level) => (
+                            <>
+                                <div className={`row lv-${level} justify-content-center my-2`}>
+                                    {committeeLevel(level).map((member) => (
+                                        member.positionEN === 'ADVISOR' ?
+                                            (
+                                                <div className="col-xl-6 col-lg-6 col-md-12 my-3">
+                                                    <div className="d-flex people-list ADVISOR px-3 py-4 align-items-center">
+                                                        <Image className="rounded-circle my-2" src={member.img} />
+                                                        <div className="w-100  m-auto">
+                                                            <h4 className=" text-uppercase ">
+                                                                {member.positionEN}
+                                                                {member.positionTH}
+                                                            </h4>
+                                                            <h6 className="bold">
+                                                                {member.nameTH}
+                                                            </h6>
+                                                            <p className="title text-reset">
+                                                                {member.detailTH}
+                                                            </p>
+                                                            <h4 className=" text-uppercase ">
+                                                                {member.nameEN}
+                                                            </h4>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            ) :
+                                            (
+                                                <div className="col-xl-3 col-lg-3 col-md-4 col-6 col-md-6 col-12  my-3">
+                                                    <div className="p-2 w-100 people-list px-2 py-4">
+                                                        <h5 className="text-uppercase bold">
+                                                            {member.positionEN}
+                                                            <br />
+                                                            {member.positionTH}
+                                                        </h5>
+                                                        <Image className="rounded-circle my-2" src={member.img} alt={member.nameEN} />
+                                                        <div className="name">
+                                                            <h6 className="bold mb-0">{member.nameTH}</h6>
+                                                            <p className="mb-0">{member.nameEN}</p>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            )
+                                    ))}
                                 </div>
-                            </div>
-                            <div className="col-xl-3 col-lg-3 col-md-4 col-sm-6 col-6 col-md-6 col-sm-6">
-                                <div className="p-2 w-100 people-list px-3 py-4 my-3" >
-                                    <h5 className=" text-uppercase  bold">
-                                        PRESIDENT
-                                        <br />
-                                        ประธาน
-                                    </h5>
-                                    <Image className="rounded-circle" src="https://imagedelivery.net/QZ6TuL-3r02W7wQjQrv5DA/8e44fcbf-3a60-4941-0e69-03b080ad4800/300" />
-                                    <h6 className="bold mb-0">คุณพนอจิตต์ จิตต์สุวรรณ</h6>
-                                    <p className="f-s-5 mb-0">Panorchit Chitsuwan Waymouth</p>
-                                </div>
-                            </div>
-                            <div className="col-xl-3 col-lg-3 col-md-4 col-sm-6 col-6 col-md-6 col-sm-6">
-                                <div className="p-2 w-100 people-list px-3 py-4 my-3">
-                                    <h5 className=" text-uppercase  bold">VICE PRESIDENT
-                                        <br />
-                                        รองประธาน
-                                    </h5>
-                                    <Image className="rounded-circle" src="https://imagedelivery.net/QZ6TuL-3r02W7wQjQrv5DA/230ea2e9-44c9-434f-96ec-649076385900/300" />
-                                    <h6 className="bold mb-0">คุณบัณฑิตทัศน์ ทสยันไชย</h6>
-                                    <p className="f-s-5 mb-0">Bandidtat Tosayanchai</p>
-                                </div>
-                            </div>
-                        </div>
-                        <hr />
-                        <div className="row">
-                            <div className="col-xl-3 col-lg-3 col-md-12 col-sm-12 col-xs-12">
-                                <div className="row  lv-2">
-                                    <div className="col-lg-12 col-md-6 col-sm-6 col-6">
-                                        <div className="p-2 w-100 people-list px-3 py-4 my-3" >
-                                            <h5 className=" text-uppercase  bold">
-                                                MANAGER
-                                                <br />
-                                                กรรมการ / ผู้จัดการ
-                                            </h5>
-                                            <Image className="rounded-circle" src="https://imagedelivery.net/QZ6TuL-3r02W7wQjQrv5DA/00b98078-2f76-4e24-abdd-d788e11a7700/300" />
-                                            <p className="bold mb-0">คุณณัฏฐา ชื่นชม</p>
-                                            <p className="f-s-5 mb-0">Nattha  Chuenchom</p>
-                                        </div>
-                                    </div>
-                                    <div className="col-lg-12 col-md-6 col-sm-6 col-6">
-                                        <div className="p-2 w-100 people-list px-3 py-4 my-3" >
-                                            <h5 className=" text-uppercase bold f-s-1-rem">
-                                                ASSISTANT MANAGER
-                                                <br />
-                                                ผู้ช่วยผู้จัดการ
-                                            </h5>
-                                            <Image className="rounded-circle" src="https://imagedelivery.net/QZ6TuL-3r02W7wQjQrv5DA/87888cc2-a8b6-488e-2fa1-59cb505ee700/300" />
-                                            <p className="bold mb-0">คุณอดิศักดิ์ ก่อแก้ว</p>
-                                            <p className="f-s-5 mb-0">Adisak Korkeaw</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-xl-9 col-lg-9 col-md-12">
-                                <div className="row lv-3">
-                                    <div className="col-lg-3 col-md-4 col-sm-4 col-6">
-                                        <div className="p-2 people-list px-3 py-4 my-3">
-                                            <h6 className="bold mb-0">CHEIF</h6>
-                                            <p className="bold mb-0"><span>หัวหน้าศูนย์หนองบุญมาก</span></p>
-                                            <Image className="rounded-circle" src="https://imagedelivery.net/QZ6TuL-3r02W7wQjQrv5DA/30bea057-8045-44e8-1254-574b24cda800/300" />
-                                            <h6 className="bold mb-0 f-s-10">คุณประวิทย์ ขุมเงิน</h6>
-                                            <p className="f-s-5 mb-0 f-s-5">Prawit  Kumngoen</p>
-                                        </div>
-                                    </div>
-                                    <div className="col-lg-3 col-md-4 col-sm-4 col-6">
-                                        <div className="p-2 people-list px-3 py-4 my-3">
-                                            <h6 className="bold mb-0">VETERINARY</h6>
-                                            <p className="bold mb-0"><span>สัตวแพทย์</span></p>
-                                            <Image className="rounded-circle" src="https://imagedelivery.net/QZ6TuL-3r02W7wQjQrv5DA/408dd58d-54ac-4e51-70eb-396d54ca3f00/300" />
-                                            <h6 className="bold mb-0 f-s-10">ดร.สถาพร อันอามาตย์</h6>
-                                            <p className="f-s-5 mb-0 f-s-5">Dr.Sathaporn Anammat</p>
-                                        </div>
-                                    </div>
-                                    <div className="col-lg-3 col-md-4 col-sm-4 col-6">
-                                        <div className="p-2 people-list px-3 py-4 my-3">
-                                            <h6 className="bold mb-0">DIRECTOR</h6>
-                                            <p className="bold mb-0"><span>กรรมการมูลนิธิบ้านหลังใหม่</span></p>
-                                            <Image className="rounded-circle" src="https://imagedelivery.net/QZ6TuL-3r02W7wQjQrv5DA/1b46085c-1d13-4c24-64d1-d3168bd9ef00/300" />
-                                            <h6 className="bold mb-0 f-s-10">คุณยนัสฐา นาคเกิด</h6>
-                                            <p className="f-s-5 mb-0 f-s-5">Chanutha Narkgerd</p>
-                                        </div>
-                                    </div>
-                                    <div className="col-lg-3 col-md-4 col-sm-4 col-6">
-                                        <div className="p-2 people-list px-3 py-4 my-3">
-                                            <h6 className="bold mb-0">DIRECTOR</h6>
-                                            <p className="bold mb-0"><span>กรรมการมูลนิธิบ้านหลังใหม่</span></p>
-                                            <Image className="rounded-circle" src="https://imagedelivery.net/QZ6TuL-3r02W7wQjQrv5DA/87a0e2b3-543d-425d-df37-8f7abb1e1400/300" />
-                                            <h6 className="bold mb-0 f-s-10">คุณสุดา เหรียญมณี</h6>
-                                            <p className="f-s-5 mb-0 f-s-5">Suda Rianmanee</p>
-                                        </div>
-                                    </div>
-                                    <div className="col-lg-3 col-md-4 col-sm-4 col-6">
-                                        <div className="p-2 people-list px-3 py-4 my-3">
-                                            <h6 className="bold mb-0">PUCLIC RELATION</h6>
-                                            <p className="bold mb-0"><span>ฝ่ายประชาสมพันธ์</span></p>
-                                            <Image className="rounded-circle" src="https://imagedelivery.net/QZ6TuL-3r02W7wQjQrv5DA/e76f3435-3e41-4f79-3300-dc91d6571000/300" />
-                                            <h6 className="bold mb-0 f-s-10">คุณลักษณาวดี สินทวงศ์ ณ อยุทธยา</h6>
-                                            <p className="f-s-5 mb-0 f-s-5">Luxanavadee Snidwongs</p>
-                                        </div>
-                                    </div>
-                                    <div className="col-lg-3 col-md-4 col-sm-4 col-6">
-                                        <div className="p-2 people-list px-3 py-4 my-3">
-                                            <h6 className="bold mb-0">PUCLIC RELATION</h6>
-                                            <p className="bold mb-0"><span>ฝ่ายประชาสมพันธ์</span></p>
-                                            <Image className="rounded-circle" src="https://imagedelivery.net/QZ6TuL-3r02W7wQjQrv5DA/d3130374-84ad-41ba-2aa2-15e293425100/300" />
-                                            <h6 className="bold mb-0 f-s-10">คุณลลิตา สินทวงศ์ ณ อยุทธยา</h6>
-                                            <p className="f-s-5 mb-0 f-s-5">Lalda Snidwongs</p>
-                                        </div>
-                                    </div>
-                                    <div className="col-lg-3 col-md-4 col-sm-4 col-6">
-                                        <div className="p-2 people-list px-3 py-4 my-3">
-                                            <h6 className="bold mb-0">PUCLIC RELATION</h6>
-                                            <p className="bold mb-0"><span>ฝ่ายประชาสมพันธ์</span></p>
-                                            <Image className="rounded-circle" src="https://imagedelivery.net/QZ6TuL-3r02W7wQjQrv5DA/8dc94b37-623b-41ce-9214-fa6e23596e00/300" />
-                                            <h6 className="bold mb-0 f-s-10">ไทยแอนนา เวย์เม้าท์</h6>
-                                            <p className="f-s-5 mb-0 f-s-5">Thaina Weymouth</p>
-                                        </div>
-                                    </div>
-                                    <div className="col-lg-3 col-md-4 col-sm-4 col-6">
-                                        <div className="p-2 people-list px-3 py-4 my-3">
-                                            <h6 className="bold mb-0">PUCLIC RELATION</h6>
-                                            <p className="bold mb-0"><span>ฝ่ายประชาสมพันธ์</span></p>
-                                            <Image className="rounded-circle" src="https://imagedelivery.net/QZ6TuL-3r02W7wQjQrv5DA/3839f5a4-fbb7-4a45-2223-55a0d2178900/300" />
-                                            <h6 className="bold mb-0 f-s-10">คุณคณิษฐ์นันท์ อิศรางกูร ณ อยุทธยา</h6>
-                                            <p className="f-s-5 mb-0 f-s-5">Kanisnan Isarangkun</p>
-                                        </div>
-                                    </div>
-                                    <div className="col-lg-3 col-md-4 col-sm-4 col-6">
-                                        <div className="p-2 people-list px-3 py-4 my-3">
-                                            <h6 className="bold mb-0">TREASURER</h6>
-                                            <p className="bold mb-0"><span>เหรัญญิก</span></p>
-                                            <Image className="rounded-circle" src=" https://imagedelivery.net/QZ6TuL-3r02W7wQjQrv5DA/67d707e8-8cd8-48d5-291b-13822fab8700/300" />
-                                            <h6 className="bold mb-0 f-s-10">คุณณัฐฐาพร เงินคำ</h6>
-                                            <p className="f-s-5 mb-0 f-s-5">Natthaphon Ngoenkham</p>
-                                        </div>
-                                    </div>
-                                    <div className="col-lg-3 col-md-4 col-sm-4 col-6">
-                                        <div className="p-2 people-list px-3 py-4 my-3">
-                                            <h6 className="bold mb-0">WEBSITE</h6>
-                                            <p className="bold mb-0"><span>ดูแลเว็บไซต์</span></p>
-                                            <Image className="rounded-circle" src="https://imagedelivery.net/QZ6TuL-3r02W7wQjQrv5DA/98d3128c-7d0b-42dd-4767-4794ee05c000/300" />
-                                            <h6 className="bold mb-0 f-s-10">คุณทศพร ดีสุทธิ</h6>
-                                            <p className="f-s-5 mb-0 f-s-5">Tossapom Doesutti</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="member">
-                            <div className=" lv-3">
-
-                            </div>
-                        </div>
+                                <hr />
+                            </>
+                        ))}
                     </div>
                 </section>
             </LayOut >
         </>
     )
 }
+export default Committee;
