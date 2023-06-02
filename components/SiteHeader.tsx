fd28211e3dfc3462e065972e4e7ee9d13df6798b
Ploy <117616170+yaminP@users.noreply.github.com>
2023-06-02 13:02:19 +0700

    modals, sitehead

--- components/SiteHeader.tsx
+++ components/SiteHeader.tsx
@@ -1,33 +1,46 @@
-import { Container, Row, Col } from "react-bootstrap";
+import { useState } from 'react';
+import { Container, Row, Col, Modal, Button } from "react-bootstrap";
 import { BsFacebook, BsTelephoneFill } from "react-icons/bs";
 import { IoIosMail } from "react-icons/io";
+import DonationModal from './modals';
 
-const SiteHeader = () => {
+
+export default function SiteHeader() {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
     return (
-        <div className="site-headr container-fluid text-light px-0 py-2">
-        <div className="row gx-0 d-none d-lg-flex">
-            <div className="col-lg-7 px-5 text-start">
-                <div className="h-100 d-inline-flex align-items-center me-4">
-                    <BsFacebook/>
-                    <span className="me-2"></span>
-                    <a href="https://web.facebook.com/profile.php?id=100057487088021">มูลนิธิบ้านหลังใหม่</a>
+        <div className="site-header container-fluid text-light px-0 py-2">
+            <div className="row gx-0 d-none d-lg-flex">
+                <div className="col-lg-7 px-5 text-start">
+                    <div className="h-100 d-inline-flex align-items-center me-4">
+                        <BsFacebook />
+                        <span className="me-2"></span>
+                        <a href="https://web.facebook.com/profile.php?id=100057487088021">มูลนิธิบ้านหลังใหม่</a>
+                    </div>
+                    <div className="h-100 d-inline-flex align-items-center">
+                        <IoIosMail />
+                        <span className="me-2"></span>
+                        <a href="mailto:info@homeagaindoggiesfoundations.com">info@homeagaindoggiesfoundations.com</a>
+                    </div>
                 </div>
-                <div className="h-100 d-inline-flex align-items-center">
-                    <IoIosMail/>
-                    <span className="me-2"></span>
-                    <a href="mailto:info@homeagaindoggiesfoundations.com">info@homeagaindoggiesfoundations.com</a>
+                <div className="col-lg-5 px-5 text-end">
+                    <div className="h-100 d-inline-flex align-items-center mx-4">
+                        <DonationModal/>
+                    </div>
+                    <div className="h-100 d-inline-flex align-items-center mx-n2">
+                        <BsTelephoneFill />
+                        <span className="me-2"></span>
+                        <a href="tel:+098-241-8799">098-241-8799</a>
+                    </div>
                 </div>
             </div>
-            <div className="col-lg-5 px-5 text-end">
-                <div className="h-100 d-inline-flex align-items-center mx-n2">
-                    <BsTelephoneFill/>
-                    <span className="me-2"></span>
-                    <a href="tel:+098-241-8799">098-241-8799</a>
-                </div>
-            </div>
+
+            
         </div>
-    </div>
     );
 };
 
-export default SiteHeader;
+
--- src/pages/supportus.tsx
+++ src/pages/supportus.tsx
@@ -6,46 +6,8 @@ export default function SupportUsPage() {
             <LayOut>
                 <div className="supportus-page container-fluid py-5 mb-5" data-wow-delay="0.1s">
                     <div className="container text-center py-5">
-                        <h1 className="display-3 mb-5">ร่วมบริจาคเพื่อสนับสนุนมูลนิธิ</h1>
+                        <h1 className="display-5 mb-5">ร่วมบริจาคเพื่อสนับสนุนมูลนิธิ</h1>
                     </div>
-                    <div className="container">
-                        <h2 className="mb-5">วิธีให้ความช่วยเหลือสนับสนุนมูลนิธิฯ</h2>
-                        <div className="row mb-5 box">
-                            <div className="col-lg-6 " data-wow-delay="0.1s">
-                                <img className=" w-75" src="/images/pic1.jpg" alt="" />
-                            </div>
-                            <div className="col-lg-6 col-md-7 boxdetail">
-                                <h1 className="mb-4">ร่วมเป็นจิตอาสา</h1>
-                                <p className="mb-4">โครงการนี้เริ่มขึ้นจากการรวมตัวกับเพื่อนๆ คนรักสัตว์ตั้งเป็นกลุ่มเล็กๆ  ชื่อ “กลุ่มเพื่อนข้างถนน เพื่อจิตอาสา โคราช”  เป็นการรวมตัวเพื่อนๆ จากหลากหลายสาขาอาชีพ  กลุ่มเราตั้งขึ้นมาเพราะอยากจะทำอะไรช่วยเหลือสังคมในแบบที่เรารักและถนัด  และทำไหว  ทำงานอาสานี้ในเวลาว่าง</p>
-                            </div>
-                        </div>
-                        
-                        <div className="row mb-5 box ">
-                            <div className="col-lg-6 " data-wow-delay="0.1s">
-                                <img className="w-75" src="/images/pic1.jpg" alt="" />
-                            </div>
-                            <div className="col-lg-6 col-md-7 boxdetail">
-                                <h1 className="mb-4">ร่วมบริจาคสิ่งของจำเป็น</h1>
-                                <p className="mb-4">สามารถร่วมบริจาคอาหารเม็ด อุปกรณ์ หรือเครื่องใช้ต่าง ๆ ที่จำเป็นสำหรับสุนัขได้ </p>
-                                <p>ติดต่อ คุณนัท: <strong>098-241-8799</strong> </p>
-                            </div>
-                        </div>
-
-                        <div className="row mb-5 box">
-                            <div className="col-lg-6 " data-wow-delay="0.1s">
-                                <img className="w-75" src="/images/pic1.jpg" alt="" />
-                            </div>
-                            <div className="col-lg-6 col-md-7 boxdetail">
-                                <h1 className="mb-4">ร่วมบริจาคเงินทำบุญ</h1>
-                                <p className="mb-2">ร่วมบริจาคเงิน เพื่อช่วยสนับสนุนค่าใช้จ่าย ในการดูแลสุนัข อาทิ ค่าอาหาร ค่ายารักษาฯ</p>
-                                <p><strong>ช่องทางการบริจาค</strong></p>
-                                <p>บัญชีธนาคารกรุไทย ชื่อบัญชี <strong>มูลนิธิบ้านหลังใหม่</strong></p>
-                                <p>เลขที่ <strong>679-3-47783-8</strong></p>
-                            </div>
-                        </div>
-
-                    </div>
-
                 </div>
             </LayOut>
         </>
