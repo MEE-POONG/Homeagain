63c6a79d424a92a43e2e954fc684b255403971d4
devilzeros <devilzeros@outlook.co.th>
2023-06-02 15:44:03 +0700

    ำหะ

--- container/Home/Sponsorship.tsx
+++ container/Home/Sponsorship.tsx
@@ -3,23 +3,23 @@ export default function Sponsorship(){
         <section className="sponsorship container text-center">
             <h3 className="mb-5">ผู้สนับสนุน</h3>
             <div className="row align-content-around">
-                <div className="col-md text-center mx-auto zoom">
-                    <img className="w-75 banner m-2" src="/images/sponsor/5-1.png" alt="" />
+                <div className="col-md text-center mx-auto">
+                    <img className="w-75 banner zoom m-2" src="/images/sponsor/5-1.png" alt="" />
                 </div>
-                <div className="col-md text-center mx-auto zoom">
-                    <img className="w-75 banner m-2" src="/images/sponsor/6.png" alt="" />
+                <div className="col-md text-center mx-auto">
+                    <img className="w-75 banner zoom m-2" src="/images/sponsor/6.png" alt="" />
                 </div>
-                <div className="col-md text-center mx-auto zoom">
-                    <img className="w-75 banner m-2" src="/images/sponsor/7.png" alt="" />
+                <div className="col-md text-center mx-auto">
+                    <img className="w-75 banner zoom m-2" src="/images/sponsor/7.png" alt="" />
                 </div>
-                <div className="col-md text-center mx-auto zoom">
-                    <img className="w-75 banner m-2" src="/images/sponsor/8.png" alt="" />
+                <div className="col-md text-center mx-auto">
+                    <img className="w-75 banner zoom m-2" src="/images/sponsor/8.png" alt="" />
                 </div>
-                <div className="col-md text-center mx-auto zoom">
-                    <img className="w-75 banner m-2" src="/images/sponsor/11-1.png" alt="" />
+                <div className="col-md text-center mx-auto">
+                    <img className="w-75 banner zoom m-2" src="/images/sponsor/11-1.png" alt="" />
                 </div>
-                <div className="col-md text-center mx-auto zoom">
-                    <img className="w-75 banner m-2" src="/images/sponsor/Untitled-design-1-1.png" alt="" />
+                <div className="col-md text-center mx-auto">
+                    <img className="w-75 banner zoom m-2" src="/images/sponsor/Untitled-design-1-1.png" alt="" />
                 </div>
             </div>
         </section>
