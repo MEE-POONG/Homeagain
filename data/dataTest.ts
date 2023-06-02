0c9df9995f261f426b9f6006f11f5712d7fec197
Ploy <117616170+yaminP@users.noreply.github.com>
2023-06-02 15:57:54 +0700

    data

--- data/dataTest.ts
+++ data/dataTest.ts
@@ -8,7 +8,8 @@ export const dogDetail = [
 export const slider = [
     {id:'1', img:'/images/7-scaled.jpg'},
     {id:'2', img:'/images/1-scaled.jpg'},
-    {id:'3', img:'/images/2-scaled.jpg'}
+    {id:'3', img:'/images/2-scaled.jpg'},
+    {id:'4', img:'/images/design.png'}
 ];
 
 export const gallery = [
