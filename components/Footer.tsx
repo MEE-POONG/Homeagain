e20a655e3dbbbbfad009475897045f95830e2ae4
Ploy <117616170+yaminP@users.noreply.github.com>
2023-06-02 17:11:20 +0700

    บริจาค

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -10,33 +10,30 @@ const Footer: React.FC = () => {
             <div className="container-fluid text-light footer mt-5 py-2 wow fadeIn" data-wow-delay="0.1s">
                 <div className="container py-5">
                     <div className="row ">
-                        <div className="col-md-6 text-break" >
-                                        <h5 className="text-white mb-4 ">ข้อมูลติดต่อ</h5>
-                                        <p className="d-flex mb-2 text-white">
-                                            <i className="me-2"><BiHomeHeart /></i>
-                                            Office 207/11 ถ.วัชรสฤษดิ์ ต.ในเมือง อ.เมือง จ.นครราชสีมา 3000
-                                        </p>
-                                        <p className="d-flex mb-2 text-white">
-                                            <i className="me-2"><BiPhone /></i>
-                                                098-241-8799
-                                        </p>
-                                        <p className="d-flex mb-2 text-white text-break">
-                                            <i className=" me-2"><IoIosMail /></i>
-                                                PCW@homeagaindoggiesfoundation.com
-                                        </p>
-
-                            <div className="d-flex pt-2">
-                                <a className="btn btn-square btn-outline-light rounded-circle me-2" href="https://www.facebook.com/profile.php?id=100057487088021"><i><FaFacebookF /></i></a>
-                            </div>
+                        <div className="col-md-2 py-2 mb-4 text-center">
+                        <img className="footlogo" src="images/logo2.jpg" alt="" />
                         </div>
-                        <div className="col-md-3 py-2 mt-5 mt-lg-0">
-                            <h5 className="text-white mb-2">หน้า</h5>
-                            <a className="btn btn-link text-white" href="">หน้าหลัก</a>
-                            <a className="btn btn-link text-white" href="">เกี่ยวกับมูลนิธิ</a>
-                            <a className="btn btn-link text-white" href="">สนับสนุนเรา</a>
-                            <a className="btn btn-link text-white" href="">การอุปถัมภ์</a>
-                            <a className="btn btn-link text-white" href="">การรับอุปการะ</a>
-                            <a className="btn btn-link text-white" href="">ติดต่อเรา</a>
+                        <div className="col-md-7 text-break" >
+                            <h5 className="text-white mb-4 ">ข้อมูลติดต่อ</h5>
+                            <p className="d-flex mb-2 text-white">
+                                <i className="me-2"><BiHomeHeart /></i>
+                                Office 207/11 ถ.วัชรสฤษดิ์ ต.ในเมือง อ.เมือง จ.นครราชสีมา 3000
+                            </p>
+                            <p className="d-flex mb-2 text-white">
+                                <i className="me-2"><BiPhone /></i>
+                                098-241-8799
+                            </p>
+                            <p className="d-flex mb-2 text-white text-break">
+                                <i className=" me-2"><IoIosMail /></i>
+                                PCW@homeagaindoggiesfoundation.com
+                            </p>
+                            <a href="https://www.facebook.com/profile.php?id=100057487088021">
+                                <p className="d-flex mb-2 text-white text-break">
+                                <i className=" me-2"><FaFacebookF /></i>
+                                มูลนิธิบ้านหลังใหม่
+                            </p>
+                            </a>
+
                         </div>
 
                         <div className="col-md-3">
--- src/pages/supportus.tsx
+++ src/pages/supportus.tsx
@@ -6,7 +6,31 @@ export default function SupportUsPage() {
             <LayOut>
                 <div className="supportus-page container-fluid py-5 mb-5" data-wow-delay="0.1s">
                     <div className="container text-center py-5">
-                        <h1 className="display-5 mb-5">ร่วมบริจาคเพื่อสนับสนุนมูลนิธิ</h1>
+                        <h2 className="display-5 mb-5">วิธีร่วมบริจาค</h2>
+
+                        <div className="text-md-start">
+                            <h4>ท่านสามารถบริจาคอาหารสัตว์และสิ่งของอื่น ๆ ที่จำเป็นต่อการดูแลสัตว์ ดังนี้</h4>
+                            {/* <div className="mt-4">
+                                <h5>สิ่งของที่ต้องการด่วน (ใช้ต่อเนื่อง)</h5>
+                                <p className="mb-0">-อาหารกระป๋อง ทุกรสชาติไม่จำกัดยี่ห้อ</p>
+                                <p className="mb-0">-อาหารเม็ด ทุกรสชาติไม่จำกัดยี่ห้อ</p>
+                                <p className="mb-0">-อาหารกระป๋อง</p>
+                            </div> */}
+                            <div className="mt-4 mb-3">
+                                <h5>สิ่งของที่รับบริจาค (ใช้ต่อเนื่อง) * รับ บริจาค ทุกยี่ห้อ *</h5>
+                                <p className="mb-0">-อาหารเม็ด สุนัข และ แมว (ทุกยี่ห้อ) สำหรับสุนัขโต</p>
+                                <p className="mb-0">-อาหารเม็ด ทุกรสชาติไม่จำกัดยี่ห้อ</p>
+                                <p className="mb-0">-แผ่นรองซับ  แบบแผ่นปูกรง</p>
+                                <p className="mb-0">-ทรายแมว (เพื่อเอาให้แมวขับถ่าย)</p>
+                                <p className="mb-0">-น้ำยาล้างจาน , ถุงขยะ ขนาดกลางและใหญ่</p>
+                                <p className="mb-0">-แชมพูอาบน้ำสุนัข</p>
+                            </div>
+                            <p className="mb-0">ท่านสามารถร่วมบริจาคเป็นสิ่งของอื่น ๆ นอกจากรายการข้างต้นได้</p>
+                            <p>ซึ่งการร่วมบริจาค สามารถติดต่อเจ้าหน้าที่มูลนิธิ คุณนัท: 098-241-8799 เพื่อดำเนินการ</p>
+
+                        </div>
+
+
                     </div>
                 </div>
             </LayOut>
