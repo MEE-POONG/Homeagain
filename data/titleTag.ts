ea8f3702b7d21e79044b3ad9de1b495d84fe2ecb
devilzeros <devilzeros@outlook.co.th>
2023-06-03 09:49:11 +0700

    est

---
+++ data/titleTag.ts
@@ -0,0 +1,7 @@
+
+export const NavigationBarData = [
+    { id: '1', img: '/images/7-scaled.jpg' },
+    { id: '2', img: '/images/1-scaled.jpg' },
+    { id: '3', img: '/images/2-scaled.jpg' },
+    { id: '4', img: '/images/design.png' }
+];
