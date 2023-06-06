9d3a4819feaf3b422001f6e858d611c8634001ee
devilzeros <devilzeros@outlook.co.th>
2023-06-06 14:15:46 +0700

    est

--- container/Home/Top-Feature.tsx
+++ container/Home/Top-Feature.tsx
@@ -1,46 +1,52 @@
+import { useContext } from 'react';
+
+import { LanguageContext } from '../../components/LanguageContext';
+
 export default function TopFeatures() {
+    const { currentLanguage } = useContext(LanguageContext);
+
     return (
         <>
-        <section>
-        <div className="container">
-                <div className="row justify-content-around">
-                    <div className="col-lg-10 col-12 text-center mx-auto mt-5">
-                        <h2 className="mb-5">ยินดีต้อนรับสู่มูลนิธิบ้านหลังใหม่</h2>
-                    </div>
-
-                    <div className="col-lg-3 col-md-6 col-12 mb-4 mb-lg-0">
-                        <div className="featured-block d-flex justify-content-center align-items-center">
-                            <a href="patronage" className="d-block">
-                                <img src="images/icon/love.png" className="featured-block-image img-fluid" alt="" />
-
-                                <p className="featured-block-text">ที่พัก <strong>สุนัขจรจัด</strong></p>
-                            </a>
+            <section>
+                <div className="container">
+                    <div className="row justify-content-around">
+                        <div className="col-lg-10 col-12 text-center mx-auto mt-5">
+                            <h2 className="mb-5">{currentLanguage === "EN" ? "Welcome to the new house foundation" : "ยินดีต้อนรับสู่มูลนิธิบ้านหลังใหม่"}</h2>
                         </div>
-                    </div>
 
-                    <div className="col-lg-3 col-md-6 col-12 mb-4 mb-lg-0 mb-md-4">
-                        <div className="featured-block d-flex justify-content-center justify-content-around align-items-center">
-                            <a href="patronage" className="d-block">
-                                <img src="images/icon/animal-care.png" className="featured-block-image img-fluid" alt="" />
+                        <div className="col-lg-3 col-md-6 col-12 mb-4 mb-lg-0">
+                            <div className="featured-block d-flex justify-content-center align-items-center">
+                                <a href="patronage" className="d-block">
+                                    <img src="images/icon/love.png" className="featured-block-image img-fluid" alt="" />
 
-                                <p className="featured-block-text"><strong>การรับอุปการะ</strong> สุนัข</p>
-                            </a>
+                                    <p className="featured-block-text">ที่พัก <strong>สุนัขจรจัด</strong></p>
+                                </a>
+                            </div>
                         </div>
-                    </div>
 
-                    <div className="col-lg-3 col-md-6 col-12 mb-4 mb-lg-0 mb-md-4">
-                        <div className="featured-block d-flex justify-content-center align-items-center">
-                            <a href="supportus" className="d-block">
-                                <img src="images/icon/donation.png" className="featured-block-image img-fluid" alt="" />
+                        <div className="col-lg-3 col-md-6 col-12 mb-4 mb-lg-0 mb-md-4">
+                            <div className="featured-block d-flex justify-content-center justify-content-around align-items-center">
+                                <a href="patronage" className="d-block">
+                                    <img src="images/icon/animal-care.png" className="featured-block-image img-fluid" alt="" />
 
-                                <p className="featured-block-text">ช่วยเราด้วยการ<strong>บริจาค</strong></p>
-                            </a>
+                                    <p className="featured-block-text"><strong>การรับอุปการะ</strong> สุนัข</p>
+                                </a>
+                            </div>
                         </div>
+
+                        <div className="col-lg-3 col-md-6 col-12 mb-4 mb-lg-0 mb-md-4">
+                            <div className="featured-block d-flex justify-content-center align-items-center">
+                                <a href="supportus" className="d-block">
+                                    <img src="images/icon/donation.png" className="featured-block-image img-fluid" alt="" />
+
+                                    <p className="featured-block-text">ช่วยเราด้วยการ<strong>บริจาค</strong></p>
+                                </a>
+                            </div>
+                        </div>
+
                     </div>
-                
                 </div>
-            </div>
-        </section>
+            </section>
         </>
         // <div className="container-fluid top-feature py-5 pt-lg-0">
         //     <div className="container py-5 pt-lg-0">
--- src/pages/patronage/index.tsx
+++ src/pages/patronage/index.tsx
@@ -1,44 +1,41 @@
 import { Button, Card } from "react-bootstrap";
 import LayOut from "../../../components/LayOut";
-import { dogDetailTH } from "../../../data/dataTest";
 
-
-export default function PatronagePage() {
-    return (
-        <>
-            <LayOut>
-                <div className="adoption-page container-fluid py-5 fade-in" data-wow-delay="0.1s">
-                    <div className=" container text-center py-3 mb-5 mt-5">
-                        <div className="mb-5">
-                            <h3 className="display-5 mb-4">การรับอุปถัมภ์สุนัขด้อยโอกาส</h3>
-                            <img src="/images/1.jpeg" alt="" className="w-100 rounded-3" />
-                        </div>
-                        
-                        <div className="row ">
-                            <p className="display-5 text-start py-4">สุนัขที่รอการอุปการะ</p>
-                            {dogDetailTH.map((item) => (
-                                <div key={item.id} className="col-lg-3 mb-4 d-flex d-sm-flex justify-content-between">
-                        
-                                    <Card  style={{ width: '15rem' }}>
-                                        <Card.Img variant="top" src={item.image} />
-                                        <Card.Body>
-                                            <Card.Title>เพศเมีย</Card.Title>
-                                            <Card.Text>
-                                                {item.old}
-                                            </Card.Text>
-                                            <a href={"patronage/" + item.id}>
-                                                <Button variant="">โปรดอุปถัมภ์หนู</Button>
-                                            </a>
-                                        </Card.Body>
-                                    </Card>
-                                   
-                                </div> ))}
-                               
-
-                        </div>
-                    </div>
-                </div>
-            </LayOut>
-        </>
-    )
+// Define the type of each dog detail item
+interface DogDetail {
+  id: string;
+  image: string;
+  old: string;
 }
+
+// Assuming dogDetailTH is an array of DogDetail
+const dogDetail: DogDetail[] = /* Your data here */;
+
+const PatronagePage = (): JSX.Element => {
+  return (
+    <LayOut>
+      <div className="adoption-page container-fluid py-5 fade-in" data-wow-delay="0.1s">
+        {/* rest of your code */}
+        <div className="row ">
+          <p className="display-5 text-start py-4">สุนัขที่รอการอุปการะ</p>
+          {dogDetail.map((item: DogDetail) => (
+            <div key={item.id} className="col-lg-3 mb-4 d-flex d-sm-flex justify-content-between">
+              <Card style={{ width: '15rem' }}>
+                <Card.Img variant="top" src={item.image} />
+                <Card.Body>
+                  <Card.Title>เพศเมีย</Card.Title>
+                  <Card.Text>{item.old}</Card.Text>
+                  <a href={"patronage/" + item.id}>
+                    <Button variant="">โปรดอุปถัมภ์หนู</Button>
+                  </a>
+                </Card.Body>
+              </Card>
+            </div> 
+          ))}
+        </div>
+      </div>
+    </LayOut>
+  );
+};
+
+export default PatronagePage;
