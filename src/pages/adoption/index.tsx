a286da00cc6b4dfef9932cca90cfa863c77a0e9f
Ploy <117616170+yaminP@users.noreply.github.com>
2023-06-08 10:39:48 +0700

    adop-poi

--- src/pages/adoption/index.tsx
+++ src/pages/adoption/index.tsx
@@ -1,8 +1,25 @@
 import { Button, Card } from "react-bootstrap";
 import LayOut from '../../../components/LayOut';
 import { dogDetail } from "../../../data/dataTest";
+import { LanguageContext } from "@components/LanguageContext";
+import { useContext } from "react";
+
+interface DogDetail {
+    id: string;
+    image: string;
+    dogName: string;
+    character: string;
+    sex: string;
+    dateofbirth?: string;
+    old: string;
+    story: string;
+    dateCome?: string;
+}
 
 export default function AdoptionPage() {
+    const { currentLanguage } = useContext(LanguageContext);
+    let myDogDetail: DogDetail[] = dogDetail.filter(dog => dog.language === currentLanguage);
+
     return (
         <>
             <LayOut>
@@ -10,12 +27,20 @@ export default function AdoptionPage() {
                 <div className="adoption-page container-fluid py-5 fade-in" data-wow-delay="0.1s">
                     <div className=" container text-center py-3 mb-5 mt-5">
                         <div className="mb-5">
-                            <h3 className="display-5 mb-4">การรับอุปการะสุนัขด้อยโอกาส</h3>
+                            <h3 className="display-5 mb-4">
+                                {currentLanguage === "EN"
+                                    ? "Adoption Dog"
+                                    : "การรับอุปการะสุนัขด้อยโอกาส"}
+                            </h3>
                             <img src="/images/1.jpeg" alt="" className="w-100 rounded-3" />
                         </div>
 
                         <div className="row align-content-around">
-                            <p className="display-5 text-start py-4">สุนัขที่รอการอุปการะ</p>
+                            <p className="display-5 text-start py-4">
+                                {currentLanguage === "EN"
+                                    ? "Adoption Dog"
+                                    : "สุนัขที่รอรับการอุปการะ"}
+                            </p>
                             {dogDetail.map((item) => (
                                 <div key={item.id} className="col-lg-3 mb-4 d-flex d-sm-flex justify-content-between">
                                     <Card style={{ width: '15rem' }}>
